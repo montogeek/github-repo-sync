9f3a7c2800ad2bb168ae2b9fdb2ffd0a40d5d4a1
Fernando Montoya <montogeek@gmail.com>
2015-04-11 16:42:26 -0500

    Fixed double quotes problem

--- repo-sync.sh
+++ repo-sync.sh
@@ -13,7 +13,7 @@ git_api_url="https://api.github.com/repos/$repo_name"
 
 upstream_url=$(curl -s --header "Accept: application/vnd.github.v3+json" $git_api_url | getJsonVal "['source']['html_url']")
 
-git remote add upstream (echo $upstream_url)
+git remote add upstream ${upstream_url//\"/}
 git fetch upstream
 git checkout master
 git merge upstream/master
