6d24e0372eb143729c2d84ae9061970a58f520ec
Fernando Montoya <montogeek@gmail.com>
2015-04-11 15:05:46 -0500

    Automatically get original repo url

--- repo-sync.sh
+++ repo-sync.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/env bash
-git remote add upstream ${1}
+function getJsonVal () {
+    python -c "import json,sys;sys.stdout.write(json.dumps(json.load(sys.stdin)$1))";
+}
+
+url=$(curl --header "Accept: application/vnd.github.v3+json" https://api.github.com/repos/montogeek/z19r | getJsonVal "['source']['html_url']")
+
+git remote add upstream $url
 git fetch upstream
 git checkout master
 git merge upstream/master
