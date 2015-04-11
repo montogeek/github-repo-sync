6cc0a74df50865dad9d2b7073fdf075bab0dfa68
Fernando Montoya <montogeek@gmail.com>
2015-04-11 16:16:52 -0500

    Automatically resolve repo api url

--- repo-sync.sh
+++ repo-sync.sh
@@ -3,9 +3,17 @@ function getJsonVal () {
     python -c "import json,sys;sys.stdout.write(json.dumps(json.load(sys.stdin)$1))";
 }
 
-url=$(curl --header "Accept: application/vnd.github.v3+json" https://api.github.com/repos/montogeek/z19r | getJsonVal "['source']['html_url']")
+git_origin_url=$(git config --get remote.origin.url)
 
-git remote add upstream $url
+regex="git@github.com:(.*).git"
+
+repo_name=$([[ $git_origin_url =~ $regex ]] && echo ${BASH_REMATCH[1]})
+
+git_api_url="https://api.github.com/repos/$repo_name"
+
+upstream_url=$(curl --header "Accept: application/vnd.github.v3+json" $git_api_url | getJsonVal "['source']['html_url']")
+
+git remote add upstream $upstream_url
 git fetch upstream
 git checkout master
 git merge upstream/master
