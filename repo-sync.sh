b0244146d3f39c6265a5ccae42fac8545187c8f3
Fernando Montoya <montogeek@gmail.com>
2015-05-01 11:35:12 -0500

    Added Double quote to prevent globbing and word splitting.
    According to https://github.com/koalaman/shellcheck/wiki/SC2086

--- repo-sync.sh
+++ repo-sync.sh
@@ -7,13 +7,13 @@ git_origin_url=$(git config --get remote.origin.url)
 
 regex="git@github.com:(.*).git"
 
-repo_name=$([[ $git_origin_url =~ $regex ]] && echo ${BASH_REMATCH[1]})
+repo_name=$([[ $git_origin_url =~ $regex ]] && echo "${BASH_REMATCH[1]}")
 
 git_api_url="https://api.github.com/repos/$repo_name"
 
-upstream_url=$(curl -s --header "Accept: application/vnd.github.v3+json" $git_api_url | getJsonVal "['source']['html_url']")
+upstream_url=$(curl -s --header "Accept: application/vnd.github.v3+json" "$git_api_url" | getJsonVal "['source']['html_url']")
 
-git remote add upstream ${upstream_url//\"/}
+git remote add upstream "${upstream_url//\"/}"
 git fetch upstream
 git checkout master
 git merge upstream/master
